56b07d2fcdb38f170d7898bc58d09371dbd3cee1
Winifred Frierson <winifred.dykes@hotmail.com>
2020-01-13 14:04:14 +0000

    Done.

--- lib/car.rb
+++ lib/car.rb
@@ -1,1 +1,17 @@
 require_relative "./vehicle.rb"
+
+class Car < Vehicle
+  
+  def initialize(wheel_size, number)
+    @wheel_size = wheel_size
+    @number = number
+  end
+  
+  def go
+    "VRRROOOOOOOOOOOOOOOOOOOOOOOM!!!!!"
+  end
+  
+  def fill_up_tak
+    "filling up"
+  end
+end
