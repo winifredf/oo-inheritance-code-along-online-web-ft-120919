dbd50869b4b5fec7d9273963ca7c62e1ba0d0657
Winifred Frierson <winifred.dykes@hotmail.com>
2020-01-13 14:02:48 +0000

    Automatically backed up by Learn

--- lib/car.rb
+++ lib/car.rb
@@ -2,4 +2,10 @@ require_relative "./vehicle.rb"
 
 class Car < Vehicle
   
-  def initialize 
+  def initialize(wheel_size, number)
+    @wheel_size = wheel_size
+    @number = number
+  end
+  
+  def go
+    
